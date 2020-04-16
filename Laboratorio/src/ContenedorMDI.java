6e0e6342b51ee746028c4d7547e21b3873c1fce3
LuisSosaR1999 <lsosarivas@gmail.com>
2020-04-15 19:07:46 -0600

    Se subio Modificaciones

--- Laboratorio/src/ContenedorMDI.java
+++ Laboratorio/src/ContenedorMDI.java
@@ -1,3 +1,7 @@
+
+import Mantenimientos.MantenimientoCliente;
+import Mantenimientos.MantenimientoProducto;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,8 +9,7 @@
  */
 
 
-import Mantenimiento.MantenimientoCliente;
-import Mantenimiento.MantenimientoProducto;
+
 
 /**
  *
